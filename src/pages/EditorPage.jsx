20a8a73503ddd2bda17d9e8ff335c66a542d774e
attendance1978-wq <attendance1978@gmail.com>
2025-11-24 07:18:14 +0800

    Create EditorPage.jsx

---
+++ src/pages/EditorPage.jsx
@@ -0,0 +1,11 @@
+import Editor from "../components/Editor.jsx";
+
+export default function EditorPage() {
+  return (
+    <div>
+      <h1>Code Editor</h1>
+      <a href="#/">Back</a>
+      <Editor />
+    </div>
+  );
+}
